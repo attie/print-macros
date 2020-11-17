4f46773c90ff6117a2ea381bd71fc6e14b63cc87
Attie Grande <attie@attie.co.uk>
2020-11-17 01:09:47 +0000

    add support for user-space

--- pk.h
+++ pk.h
@@ -6,6 +6,11 @@
 #define PKF(fmt, args...)  PKR(": " fmt, ##args)
 #define PKV(fmt, var)      PKR(": " #var ": " fmt, var)
 #define PKVB(fmt, var)     PKR(": " #var ": [" fmt "]", var)
-#define PKR(fmt, args...)  printk(KERN_INFO "ATTIE: %s:%d %s()" fmt, __FILE__, __LINE__, __FUNCTION__, ##args)
+
+#ifdef __KERNEL__
+#  define PKR(fmt, args...)  printk(KERN_INFO "ATTIE: %s:%d %s()" fmt, __FILE__, __LINE__, __FUNCTION__, ##args)
+#else
+#  define PKR(fmt, args...)  fprintf(stderr, "ATTIE: %s:%d %s()" fmt "\n", __FILE__, __LINE__, __FUNCTION__, ##args)
+#endif
 
 #endif /* PK_H */
